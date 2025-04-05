39432710838451ca99b9610c8a22dfd78f0cde26
asad5059 <asadpatiya3@gmail.com>
2025-04-05 13:12:07 +0600

    Add chat input box

---
+++ frontend/chat-app/src/components/ChatRoom/ChatInputBox/index.jsx
@@ -0,0 +1,11 @@
+const ChatInputBox = () => {
+    return (
+        <div className="chat-input-box">
+            <input type="text-area" name="" id="" placeholder="Enter Your Message"/>
+            <button type="submit">Send</button>
+        </div>
+    )
+};
+
+
+export default ChatInputBox;
--- frontend/chat-app/src/components/ChatRoom/index.jsx
+++ frontend/chat-app/src/components/ChatRoom/index.jsx
@@ -1,10 +1,14 @@
+import ChatInputBox from "./ChatInputBox";
 import SideBar from "./SideBar";
+
+import './index.scss';
 
 function ChatRoom() {
     return (
-        <>
+        <div className="chat-room">
             <SideBar />
-        </>
+            <ChatInputBox />
+        </div>
     );
 }
 
